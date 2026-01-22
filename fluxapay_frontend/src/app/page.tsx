018178df14371dc0f811940cc8ba7959a883d61b
Akshola00 <akinniyishola07@gmail.com>
2026-01-22 01:47:04 +0100

    remove unneccessary things

--- fluxapay_frontend/src/app/page.tsx
+++ fluxapay_frontend/src/app/page.tsx
@@ -1,16 +1,11 @@
-import { Navbar, Hero, WhyFluxapay, Bridges, GlobalReach, DailyUsage, FAQ, Footer } from "@/features/landing";
+import { WhyFluxapay, Bridges, GlobalReach } from "@/features/landing";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <Navbar />
-      <Hero />
       <WhyFluxapay />
       <Bridges />
       <GlobalReach />
-      <DailyUsage />
-      <FAQ />
-      <Footer />
     </main>
   );
 }
